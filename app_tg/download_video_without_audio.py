d510c723144106dc24e7ce1d5bdcce28c164d52c
Edvard <79434126+vbncursed@users.noreply.github.com>
2024-07-09 00:04:16 +0500

    Delete app_tg directory

--- app_tg/download_video_without_audio.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-
-
-def download_video_without_audio(url, output_path):
-    yt = YouTube(url)
-    video_stream = yt.streams.filter(
-        adaptive=True, file_extension="mp4", only_video=True
-    ).first()
-    video_stream.download(output_path=output_path)
